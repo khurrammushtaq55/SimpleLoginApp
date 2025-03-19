f3127b3a735d37c5b36e33be5c84249231f1679b
khurram <khurram_mushtaq55@yahoo.com>
2025-03-19 16:48:48 +0500

    Add: Login Functionality with validation

--- app/src/main/java/com/test/bikemapapplication/presentation/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/test/bikemapapplication/presentation/viewmodel/LoginViewModel.kt
@@ -4,7 +4,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.test.bikemapapplication.data.ApiService
+import kotlinx.coroutines.launch
+import com.test.bikemapapplication.data.Error
 
 class LoginViewModel(private val apiService: ApiService = ApiService()) : ViewModel() {
     var email by mutableStateOf("")
@@ -12,6 +15,24 @@ class LoginViewModel(private val apiService: ApiService = ApiService()) : ViewMo
     var loginResult by mutableStateOf<String?>(null)
 
     fun login() {
+        if (email.isBlank()) {
+            loginResult = "Email cannot be empty" // should be localized
+            return
+        }
+        if (password.isBlank()) {
+            loginResult = "Password cannot be empty" // should be localized
+            return
+        }
+
+        viewModelScope.launch {
+            val response = apiService.login(email, password)
+            loginResult = when {
+                response.result != null -> "Login Successful"
+                response.error == Error.WRONG_CREDENTIALS -> "Wrong Credentials"
+                response.error == Error.INTERNAL_SERVER_ERROR -> "Internal Server Error"
+                else -> "Unknown Error"
+            }
+        }
 
     }
 }
